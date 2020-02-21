b39f9a9295e54d862f05d9315c8251800e472e08
Paloma C Riera <alexis.a.riera@gmail.com>
2020-02-21 22:40:46 +0100

    segon commit amb lagarto i spock

--- ppt.js
+++ ppt.js
@@ -1,73 +1,151 @@
-//Genera un número aleatorio entre un rango de enteros
-//Això està fet amb JavaScript
-//Segon commit
-
-function patito(minimo, maximo)
+function aleatorio(minimo, maximo)
 {
-    var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );
-    return numero;
+  var numero = Math.round( Math.random() * (maximo - minimo + 1) + minimo);
+  return numero;
 }
+
+// Math es un objeto son colecciones de funciones y variables y es el paquete de opciones matematicas que tiene javascript 
+// Math.floor redondea los número decimales a enteros
+// Math.random genera un numero random entre 0 y el 1 (del 0% al 100%) menos 0 y 1
 
 var piedra = 0;
 var papel = 1;
 var tijera = 2;
+var lagarto = 3;
+var spock = 4;
 
-var opciones = ["Piedra", "Papel", "Tijera"];
+var opciones = ["Piedra", "Papel", "Tijera", "Lagarto", "Spock"];
+
 
 var opcionUsuario;
-var opcionMaquina = patito(0,2);
+var opcionMaquina = aleatorio(0,4);
 
-opcionUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
+opcionUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2\nLagarto: 3\nSpock: 4", 0)
+
 
 alert("Elegiste " + opciones[opcionUsuario]);
-alert("Javascript eligió " + opciones[opcionMaquina]);
+alert("Javascript eligio " + opciones[opcionMaquina]);
+
 
 if(opcionUsuario == piedra)
-{
-    if(opcionMaquina == piedra)
     {
-        alert("Empate!");
+
+      if(opcionMaquina == piedra)
+        {
+          alert("Empate!");
+        }
+      else if(opcionMaquina == papel)
+          {
+            alert("Perdiste");
+          }
+      else if(opcionMaquina == tijera)
+        {
+          alert("Ganaste!");
+        }
+      else if(opcionMaquina == lagarto)
+        {
+          alert("Ganaste!");
+        }
+      else if(opcionMaquina == spock)
+        {
+          alert("Ganaste!");
+        }
     }
-    else if(opcionMaquina == papel)
-    {
-        alert("Perdiste :( ");
-    }
-    else if(opcionMaquina == tijera)
-    {
-        alert("Ganaste!");
-    }
-}
 else if(opcionUsuario == papel)
-{
-    if(opcionMaquina == piedra)
     {
-        alert("Ganaste!");
+
+      if(opcionMaquina == piedra)
+        {
+          alert("Ganaste!");
+        }
+      else if(opcionMaquina == papel)
+          {
+            alert("Empate!");
+          }
+      else if(opcionMaquina == tijera)
+        {
+          alert("Perdiste!");
+        }
+      else if(opcionMaquina == lagarto)
+        {
+          alert("Perdiste!");
+        }
+      else if(opcionMaquina == spock)
+        {
+          alert("Ganaste!");
+        }
     }
-    else if(opcionMaquina == papel)
-    {
-        alert("Empate!");
-    }
-    else if(opcionMaquina == tijera)
-    {
-        alert("Perdiste!");
-    }
-}
 else if(opcionUsuario == tijera)
-{
-    if(opcionMaquina == piedra)
     {
+
+      if(opcionMaquina == piedra)
+        {
+          alert("Perdiste!");
+        }
+      else if(opcionMaquina == papel)
+          {
+            alert("Ganaste");
+          }
+      else if(opcionMaquina == tijera)
+        {
+          alert("Empate!");
+        }
+      else if(opcionMaquina == lagarto)
+        {
+          alert("Ganaste!");
+        }
+      else if(opcionMaquina == spock)
+        {
+          alert("Perdiste!");
+        }
+    }
+else if(opcionUsuario == lagarto)
+  	{
+      if(opcionMaquina == piedra)
+        {
+          alert("Perdiste!");
+        }
+      else if(opcionMaquina == papel)
+        {
+          alert("Ganaste!");
+        }
+      else if(opcionMaquina == tijera)
+        {
+          alert("Perdiste");
+        }
+      else if(opcionMaquina == lagarto)
+        {
+          alert("Empate");
+        }
+      else if(opcionMaquina == spock)
+        {
+          alert("Ganaste!");
+        }
+    }
+else if(opcionUsuario == spock)
+  	{
+      if(opcionMaquina == piedra)
+        {
+          alert("Ganaste!");
+        }
+      else if(opcionMaquina == papel)
+      {
         alert("Perdiste!");
+      }
+      else if(opcionMaquina == tijera)
+        {
+          alert("Ganaste!");
+        }
+      else if(opcionMaquina == lagarto)
+        {
+          alert("Perdiste!");
+        }
+      else if(opcionMaquina == spock)
+        {
+          alert("Empate!");
+        }
     }
-    else if(opcionMaquina == papel)
-    {
-        alert("Ganaste!");
-    }
-    else if(opcionMaquina == tijera)
-    {
-        alert("Empate!");
-    }
-}
-else
-{
-    alert("wtf?");
-}
+else if(opcionUsuario > 2)
+  {
+    alert("Vuelve a elegir");
+  }
